4c1f381bb98173b161d5c9b51559d3101739d4b3
torkvell <tor.kvell@outlook.com>
2020-03-30 19:49:06 +0200

    update to event reducer so local state reflects all server changes

--- src/redux/events/reducer.js
+++ src/redux/events/reducer.js
@@ -6,6 +6,65 @@ export default (state = initialState, action = {}) => {
       return [...action.payload];
     case "COMMENT_CREATED":
       return [...action.payload];
+    case "EVENT_CREATED":
+      const newEvent = { ...action.payload, tickets: [] };
+      return [...state, newEvent];
+    case "EVENT_DELETED":
+      const deletedEventId = action.payload;
+      const newState = state.filter(event => event.id !== deletedEventId);
+      return newState;
+    case "TICKET_CREATED":
+      const eventToUpdate = state.find(
+        event => event.id === action.payload.eventId
+      );
+      const allOtherEvents = state.filter(
+        event => event.id !== action.payload.eventId
+      );
+      //append new ticket to the corresponding event
+      const newEventTickets = [...eventToUpdate.tickets, action.payload];
+      const updatedEvent = { ...eventToUpdate, tickets: newEventTickets };
+      //append updated event to state
+      const newEvents = [...allOtherEvents, updatedEvent];
+      return newEvents;
+    case "TICKET_DELETED":
+      const deletedTicketId = action.payload;
+      const newEventArray = state.map(event => {
+        const ticket = event.tickets.find(ticket =>
+          ticket.id === deletedTicketId ? true : false
+        );
+        if (ticket) {
+          //remove deleted ticket from the corresponding event
+          const newTickets = event.tickets.filter(
+            ticket => ticket.id !== deletedTicketId
+          );
+          //append filtered ticket array to corresponding event
+          const newEvent = { ...event, tickets: newTickets };
+          return newEvent;
+        } else {
+          return event;
+        }
+      });
+      return newEventArray;
+    case "TICKET_UPDATED":
+      const updatedTicket = action.payload;
+      const updatedState = state.map(event => {
+        const ticket = event.tickets.find(ticket =>
+          ticket.id === updatedTicket.id ? true : false
+        );
+        if (ticket) {
+          //remove ticket that has been updated from the corresponding event ticket array
+          const filteredTickets = event.tickets.filter(
+            ticket => ticket.id !== updatedTicket.id
+          );
+          const newTickets = [...filteredTickets, updatedTicket];
+          //append updated ticket array to corresponding event
+          const newEvent = { ...event, tickets: newTickets };
+          return newEvent;
+        } else {
+          return event;
+        }
+      });
+      return updatedState;
     default:
       return state;
   }
