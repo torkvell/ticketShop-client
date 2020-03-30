26302bfa7a2e837cba329f9f9d8b046ad7f0ad78
torkvell <tor.kvell@outlook.com>
2020-03-30 09:36:17 +0200

    added date picker for event creation

--- src/components/user/myEvents/MyEventTable.js
+++ src/components/user/myEvents/MyEventTable.js
@@ -3,14 +3,39 @@ import MaterialTable from "material-table";
 import Container from "@material-ui/core/Container";
 
 export default function EventTable(props) {
-  console.log("MyEventTable props:", props);
+  // const onDateChange = (start, date) => {
+  //   if (start) {
+  //     setStartDate({ startDate: date });
+  //   } else if (!start) {
+  //     setEndDate({ endDate: date });
+  //   }
+  // };
+  const [startDate, setStartDate] = React.useState(null);
+  const [endDate, setEndDate] = React.useState(null);
   const [state, setState] = React.useState({
+    startDate: null,
+    endDate: null,
     columns: [
       { title: "ID", field: "id", editable: "never" },
       { title: "Name", field: "name" },
       { title: "Description", field: "description" },
-      { title: "Start date", field: "startDate" },
-      { title: "End date", field: "endDate" },
+      {
+        title: "Start date",
+        field: "startDate",
+        editComponent: () => (
+          <input
+            type="date"
+            onChange={e => setStartDate(e.target.value)}
+          ></input>
+        )
+      },
+      {
+        title: "End date",
+        field: "endDate",
+        editComponent: () => (
+          <input type="date" onChange={e => setEndDate(e.target.value)}></input>
+        )
+      },
       { title: "Image URL", field: "imageURL" }
     ],
     data: props.user.events
@@ -60,25 +85,25 @@ export default function EventTable(props) {
               props.createEvent(
                 newData.name,
                 newData.imageURL,
-                newData.startDate,
-                newData.endDate,
+                startDate,
+                endDate,
                 newData.description,
                 props.user.token
               );
             }),
-          onRowUpdate: (newData, oldData) =>
-            new Promise(resolve => {
-              setTimeout(() => {
-                resolve();
-                if (oldData) {
-                  setState(prevState => {
-                    const data = [...prevState.data];
-                    data[data.indexOf(oldData)] = newData;
-                    return { ...prevState, data };
-                  });
-                }
-              }, 600);
-            }),
+          // onRowUpdate: (newData, oldData) =>
+          //   new Promise(resolve => {
+          //     setTimeout(() => {
+          //       resolve();
+          //       if (oldData) {
+          //         setState(prevState => {
+          //           const data = [...prevState.data];
+          //           data[data.indexOf(oldData)] = newData;
+          //           return { ...prevState, data };
+          //         });
+          //       }
+          //     }, 600);
+          //   }),
           onRowDelete: oldData =>
             new Promise(resolve => {
               setTimeout(() => {
