95a1015e1dec50c6cd653e15ecc29f05b3a65528
torkvell <tor.kvell@outlook.com>
2020-04-14 14:29:03 +0200

    refactor for ticket layout and fraud rating message

--- src/components/ticket/TicketCardDesktop.jsx
+++ src/components/ticket/TicketCardDesktop.jsx
@@ -2,14 +2,12 @@ import React from "react";
 import clsx from "clsx";
 import { Link } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
-import './ticketCardDesktop.css'
+import "./ticketCardDesktop.css";
 import {
-  Card,
   CardHeader,
   CardMedia,
   CardContent,
   CardActions,
-  Collapse,
   Avatar,
   IconButton,
   Typography,
@@ -19,81 +17,87 @@ import {
   Container,
   ListItem,
   ListItemAvatar,
-  ImageIcon,
   ListItemText,
   Divider,
-  WorkIcon
 } from "@material-ui/core/";
+import Alert from "@material-ui/lab/Alert";
 import { red } from "@material-ui/core/colors";
 import FavoriteIcon from "@material-ui/icons/Favorite";
 import ShareIcon from "@material-ui/icons/Share";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   media: {
     height: 0,
-    paddingTop: "56.25%" // 16:9
+    paddingTop: "56.25%", // 16:9
   },
   expand: {
     transform: "rotate(0deg)",
     marginLeft: "auto",
     transition: theme.transitions.create("transform", {
-      duration: theme.transitions.duration.shortest
-    })
+      duration: theme.transitions.duration.shortest,
+    }),
   },
   expandOpen: {
-    transform: "rotate(180deg)"
+    transform: "rotate(180deg)",
   },
   avatar: {
-    backgroundColor: red[500]
+    backgroundColor: red[500],
   },
   priceTag: {
     fontWeight: "bold",
-    marginLeft: "50px"
+    marginLeft: "145px",
+    fontSize: "2em",
   },
   fraudRiskRed: {
     color: "red",
-    fontWeight: "bold"
+    fontWeight: "bold",
   },
   fraudRiskGreen: {
     color: "green",
-    fontWeight: "bold"
+    fontWeight: "bold",
   },
   navLinkWhite: {
     color: "white",
     textDecoration: "none !important",
     "&:hover, &:focus": {
-      color: "white"
-    }
+      color: "white",
+    },
   },
   fraudTicket: {
-    boxShadow: '2px 2px 15px 0px #FF0000'
+    boxShadow: "2px 2px 15px 0px #FF0000",
   },
   commentArea: {
-      display: 'flex',
-      flexDirection: 'column',
-      maxWidth: '500px',
-      marginTop: '-150px',
-      marginBottom: '20px'
+    display: "flex",
+    flexDirection: "column",
+    maxWidth: "500px",
+    marginTop: "-180px",
+    marginBottom: "20px",
   },
-  commentItemHeading:{
-      display: 'flex',
-      flexDirection: 'row',
-      justifyContent: 'space-between',
+  commentItemHeading: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
   commentFormWrapper: {
-      display: 'flex',
-      flexDirection: 'column',
-      width: '100%',
-      marginRight: '-15px'
+    display: "flex",
+    flexDirection: "column",
+    width: "100%",
+    marginRight: "-15px",
   },
   row: {
-    display: 'flex',
-    flexDirection: 'column',
+    display: "flex",
+    flexDirection: "column",
   },
   comment: {
-    minWidth: '380px'
-  }
+    minWidth: "380px",
+  },
+  cardActions: {
+    marginTop: "30px",
+  },
+  cardActionDescription: {
+    maxHeight: "80px",
+  },
 }));
 
 export default function TicketCard(props) {
@@ -108,116 +112,137 @@ export default function TicketCard(props) {
   const ticketEndDate = props.event.endDate.slice(0, 10);
   const ticketEndTime = props.event.endDate.substr(11, 5);
 
+  const getSeverityRatingTicket = (fraudRisk) => {
+    if (fraudRisk >= 50) {
+      return "error";
+    } else if (fraudRisk > 25 && fraudRisk < 50) {
+      return "warning";
+    } else if (fraudRisk <= 25) {
+      return "success";
+    }
+  };
+
   return (
     <div>
-    <div className="ticket">
+      <Alert severity={getSeverityRatingTicket(props.ticketData.fraudRisk)}>
+        We calculated a fraud rating of {props.ticketData.fraudRisk}%
+      </Alert>
+      <div className="ticket">
         <div className="ticketContentWrapper">
-            <div className="ticketContent">
-                <CardMedia className="ticketPicture" image={props.ticketData.imageUrl} />
-                <div className="ticketInfo">
-                    <CardContent>
-                        <CardHeader
-                            avatar={
-                             <Avatar
-                                aria-label="recipe"
-                                className={classes.avatar}
-                                image={props.ticketData.imageUrl}
-                             ></Avatar>
-                            }
-                            title={props.ticketData.title}
-                            subheader={`Ticket owner: ${props.ticketData.ownerName}`}
-                            />
-                        <Typography variant="body2" color="textSecondary" component="p">
-                        Start: {ticketStartDate} {ticketStartTime} | End: {ticketEndDate}{" "}
-                        {ticketEndTime}
-                        <br></br>
-                        <br></br>
-                        {props.ticketData.description}
-                        <br></br>
-                        <br></br>
-                        </Typography>
-                        <CardActions disableSpacing>
-                            <Button
-                            color="primary"
-                            variant="contained"
-                            fullWidth
-                            onClick={() => props.addProductToCart(props.ticketData)}
-                            >
-                            <Link className={classes.navLinkWhite} to={process.env.PUBLIC_URL + '/cart'}>
-                            BUY NOW
-                            </Link>
-                            </Button>
-                        </CardActions>
-                        <CardActions disableSpacing>
-                            <IconButton aria-label="add to favorites">
-                                <FavoriteIcon />
-                            </IconButton>
-                            <IconButton aria-label="share">
-                                <ShareIcon />
-                            </IconButton>
-                            <Typography
-                                className={
-                                    props.ticketData.fraudRisk > 50
-                                    ? classes.fraudRiskRed
-                                    : classes.fraudRiskGreen
-                                }
-                                >
-                                Fraud Rating: {props.ticketData.fraudRisk}%
-                            </Typography>
-                            <Typography className={classes.priceTag}>
-                                € {props.ticketData.price}
-                            </Typography>
-                        </CardActions>
-                    </CardContent>
-                </div>
-            </div>
-        </div>
-    </div>
-    <Container className={classes.commentArea}>
-    <h3>Comments</h3>
-    <List className={classes.root}>
-    {props.ticketData.comments.map((comment, index) => {
-            return (
-                <div>
-                    <ListItem key={index}>
-                        <ListItemAvatar>
-                            <Avatar>
-                            </Avatar>
-                        </ListItemAvatar>
-                        <div className={classes.row}>
-                            <ListItemText className={classes.commentItemHeading} primary="Vacation" secondary="July 20, 2014" />
-                            <div className={classes.comment}>{comment.comment}</div>
-                        </div>
-                    </ListItem>
-                    <Divider variant="inset" component="li" />
-                </div>
-            )
-          })}
-      <ListItem>
-        <ListItemAvatar>
-          <Avatar>
-          </Avatar>
-      </ListItemAvatar>
-        <div className={classes.commentFormWrapper}>
-            <ListItemText className={classes.commentItemHeading} primary="Vacation" secondary="July 20, 2014" />
-            <form onSubmit={props.handleSubmit}>
-            <TextField
-                id="outlined-basic"
-                label="Comment"
-                variant="outlined"
-                fullWidth
-                name="comment"
-                value={props.comment}
-                onChange={props.handleChange}
+          <div className="ticketContent">
+            <CardMedia
+              className="ticketPicture"
+              image={props.ticketData.imageUrl}
             />
-            </form>
+            <div className="ticketInfo">
+              <CardContent>
+                <CardHeader
+                  avatar={
+                    <Avatar
+                      aria-label="recipe"
+                      className={classes.avatar}
+                      image={props.ticketData.imageUrl}
+                    ></Avatar>
+                  }
+                  title={props.ticketData.title}
+                  subheader={`Ticket owner: ${props.ticketData.ownerName}`}
+                />
+                <Typography
+                  variant="body2"
+                  color="textSecondary"
+                  component="p"
+                  className={classes.cardActionDescription}
+                >
+                  Start: {ticketStartDate} {ticketStartTime} | End:{" "}
+                  {ticketEndDate} {ticketEndTime}
+                  <br></br>
+                  <br></br>
+                  {props.ticketData.description}
+                  <br></br>
+                  <br></br>
+                </Typography>
+                <CardActions disableSpacing className={classes.cardActions}>
+                  <Button
+                    color="primary"
+                    variant="contained"
+                    fullWidth
+                    onClick={() => props.addProductToCart(props.ticketData)}
+                  >
+                    <Link
+                      className={classes.navLinkWhite}
+                      to={process.env.PUBLIC_URL + "/cart"}
+                    >
+                      BUY NOW
+                    </Link>
+                  </Button>
+                </CardActions>
+                <CardActions disableSpacing className={classes.cardActions}>
+                  <IconButton aria-label="add to favorites">
+                    <FavoriteIcon />
+                  </IconButton>
+                  <IconButton aria-label="share">
+                    <ShareIcon />
+                  </IconButton>
+                  <Typography className={classes.priceTag}>
+                    € {props.ticketData.price}
+                  </Typography>
+                </CardActions>
+              </CardContent>
+            </div>
+          </div>
         </div>
-      </ListItem>
-      <Button type="submit" color="primary" variant="contained" fullWidth>
-        Comment
-      </Button>
-    </List>
-    </Container>
-</div>
+      </div>
+      <Container className={classes.commentArea}>
+        <h3>Comments</h3>
+        <List className={classes.root}>
+          {props.ticketData.comments.map((comment, index) => {
+            return (
+              <div>
+                <ListItem key={index}>
+                  <ListItemAvatar>
+                    <Avatar></Avatar>
+                  </ListItemAvatar>
+                  <div className={classes.row}>
+                    <ListItemText
+                      className={classes.commentItemHeading}
+                      primary="Vacation"
+                      secondary="July 20, 2014"
+                    />
+                    <div className={classes.comment}>{comment.comment}</div>
+                  </div>
+                </ListItem>
+                <Divider variant="inset" component="li" />
+              </div>
+            );
+          })}
+          <ListItem>
+            <ListItemAvatar>
+              <Avatar></Avatar>
+            </ListItemAvatar>
+            <div className={classes.commentFormWrapper}>
+              <ListItemText
+                className={classes.commentItemHeading}
+                primary="Vacation"
+                secondary="July 20, 2014"
+              />
+              <form onSubmit={props.handleSubmit}>
+                <TextField
+                  id="outlined-basic"
+                  label="Comment"
+                  variant="outlined"
+                  fullWidth
+                  name="comment"
+                  value={props.comment}
+                  onChange={props.handleChange}
+                />
+              </form>
+            </div>
+          </ListItem>
+          <Button type="submit" color="primary" variant="contained" fullWidth>
+            Comment
+          </Button>
+        </List>
+      </Container>
+    </div>
   );
 }
